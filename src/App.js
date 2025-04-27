6cbf9839da476de71db0139cdcea75ce73478953
Ayush Shrivastava <ayushshrivastava840@gmail.com>
2025-04-27 12:37:59 +0530

    form updated

--- src/App.js
+++ src/App.js
@@ -367,7 +367,7 @@ function App() {
           <div className="col-md-8">
             <div className={`p-4 rounded shadow ${darkMode ? "bg-dark text-white" : "bg-white text-dark"}`} data-aos="zoom-in">
               <form
-                action="https://docs.google.com/forms/d/e/1FAIpQLScxh6qpFb0TlGMyvZW03AYZrJyx0Fo0TsFibgXagguwzU8t9A/viewform?usp=header"
+                action="https://forms.gle/QDPXkFi7Xsqzc7PR6"
                 method="POST"
                 target="_blank"
               >
